74f2a182826475ac80fefae3c331a5ac2cf2f69a
AyushRamanUprety <ayushramanuprety@students.federation.edu.au>
2024-12-12 14:27:23 +1100

    https://github.com/SzzSuraj/Restaurant-Reservation-and-Ordering-System

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/BistroDB')
+mongoose.connect('mongodb://127.0.0.1:27017/BistroDB')
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
 
